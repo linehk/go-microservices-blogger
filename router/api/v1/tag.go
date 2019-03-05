f09a9459972d38d3581d6930b6a914088ac834dc
sulinehk <sulinehk@gmail.com>
2019-03-05 17:43:12 +0800

    router/api/v1: test GetTags

--- router/api/v1/tag.go
+++ router/api/v1/tag.go
@@ -1,10 +1,47 @@
 package v1
 
 import (
+	"net/http"
+
+	"github.com/linehk/gin-blog/router/api"
+
+	"github.com/Unknwon/com"
 	"github.com/gin-gonic/gin"
+	"github.com/linehk/gin-blog/vm"
 )
 
 func GetTags(c *gin.Context) {
+	name := c.Query("name")
+	state := -1
+	if s := c.Query("state"); s != "" {
+		state = com.StrTo(s).MustInt()
+	}
+
+	vmTag := vm.Tag{
+		Name:     name,
+		State:    state,
+		PageNum:  api.PageNum(c),
+		PageSize: 10,
+	}
+
+	tags, err := vmTag.GetAll()
+	if err != nil {
+		api.Response(c, http.StatusInternalServerError,
+			11, nil)
+		return
+	}
+
+	count, err := vmTag.Count()
+	if err != nil {
+		api.Response(c, http.StatusInternalServerError,
+			22, nil)
+		return
+	}
+
+	api.Response(c, http.StatusOK, 33, map[string]interface{}{
+		"lists": tags,
+		"count": count,
+	})
 }
 
 func GetTag(c *gin.Context) {
