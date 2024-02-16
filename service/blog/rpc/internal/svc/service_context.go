a9a9d1a9d0f55f3b3e43be5a742e93929ab0c651
sulinehk <sulinehk@gmail.com>
2024-02-16 13:56:24 +0800

    run make pb to generate base structure

---
+++ service/blog/rpc/internal/svc/service_context.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/linehk/go-blogger/service/blog/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
