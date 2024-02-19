2e072b787fbe4759c7c838d40f5a2533b7b960bd
sulinehk <sulinehk@gmail.com>
2024-02-19 23:37:00 +0800

    add script for build and start

---
+++ script/build.sh
@@ -0,0 +1,8 @@
+set -e
+
+for service_name in user blog post page comment
+do
+    CGO_ENABLED=0 GOOS=linux go build \
+        -o ./bin/ \
+        ./service/"$service_name"/rpc/"$service_name".go
+done
---
+++ script/start.sh
@@ -0,0 +1,6 @@
+set -e
+
+for service_name in user blog post page comment
+do
+    ./bin/"$service_name" -f ./service/"$service_name"/rpc/etc/"$service_name".yaml &
+done
