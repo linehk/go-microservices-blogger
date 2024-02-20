a79de3960bc39e0851494f8665fd110021b1a2ea
sulinehk <sulinehk@gmail.com>
2024-02-20 14:30:39 +0800

    use 2 spaces for shell script
    According to https://google.github.io/styleguide/shellguide.html

--- script/build.sh
+++ script/build.sh
@@ -4,7 +4,7 @@ set -e
 
 for service_name in user blog post page comment
 do
-    CGO_ENABLED=0 GOOS=linux go build \
-        -o ./bin/ \
-        ./service/"$service_name"/rpc/"$service_name".go
+  CGO_ENABLED=0 GOOS=linux go build \
+    -o ./bin/ \
+    ./service/"$service_name"/rpc/"$service_name".go
 done
--- script/start.sh
+++ script/start.sh
@@ -4,5 +4,6 @@ set -e
 
 for service_name in user blog post page comment
 do
-    ./bin/"$service_name" -f ./service/"$service_name"/rpc/etc/"$service_name".yaml &
+  ./bin/"$service_name" \
+  -f ./service/"$service_name"/rpc/etc/"$service_name".yaml &
 done
