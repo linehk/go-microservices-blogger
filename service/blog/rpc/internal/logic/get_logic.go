64e30f395923c197d90a300d5b01a0459dbdaf09
sulinehk <sulinehk@gmail.com>
2024-02-24 19:01:35 +0800

    feat(blog): add log to debug when error occur

--- service/blog/rpc/internal/logic/get_logic.go
+++ service/blog/rpc/internal/logic/get_logic.go
@@ -30,16 +30,22 @@ func NewGetLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetLogic {
 func (l *GetLogic) Get(in *blog.GetReq) (*blog.Blog, error) {
 	blogModel, err := l.svcCtx.BlogModel.FindOneByUuid(l.ctx, in.GetBlogId())
 	if errors.Is(err, model.ErrNotFound) {
-		return nil, fmt.Errorf("BlogModel.FindOneByUuid NotFound err: %v", err)
+		wrapErr := fmt.Errorf("BlogModel.FindOneByUuid NotFound err: %v", err)
+		l.Error(wrapErr)
+		return nil, wrapErr
 	}
 	if err != nil {
-		return nil, fmt.Errorf("BlogModel.FindOneByUuid err: %v", err)
+		wrapErr := fmt.Errorf("BlogModel.FindOneByUuid err: %v", err)
+		l.Error(wrapErr)
+		return nil, wrapErr
 	}
 
 	var blogResp blog.Blog
 	err = copier.Copy(&blogResp, blogModel)
 	if err != nil {
-		return nil, fmt.Errorf("copier.Copy err: %v", err)
+		wrapErr := fmt.Errorf("copier.Copy err: %v", err)
+		l.Error(wrapErr)
+		return nil, wrapErr
 	}
 	blogResp.Kind = "blogger#blog"
 
