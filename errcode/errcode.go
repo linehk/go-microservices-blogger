e268953d29a6cdb49229a4d20840bf955d3f9522
sulinehk <sulinehk@gmail.com>
2024-02-26 16:39:09 +0800

    feat(all): add errcode for more friendly error message

---
+++ errcode/errcode.go
@@ -0,0 +1,32 @@
+package errcode
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	Database uint32 = 20000
+	Convert  uint32 = 20001
+	Service  uint32 = 20002
+
+	UserNotExist   uint32 = 30000
+	LocaleNotExist uint32 = 30001
+)
+
+var msg = map[uint32]string{
+	Database: "database error",
+	Convert:  "convert error",
+	Service:  "service error",
+
+	UserNotExist:   "user not exist",
+	LocaleNotExist: "locale not exist",
+}
+
+func Wrap(e uint32) error {
+	return status.Error(codes.Code(e), msg[e])
+}
+
+func Msg(e uint32) string {
+	return msg[e]
+}
