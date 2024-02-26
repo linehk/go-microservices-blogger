3c64c90850081d405072efb3d33fa39b5485d307
sulinehk <sulinehk@gmail.com>
2024-02-26 17:45:22 +0800

    test(user): add mock test for Get

--- service/user/rpc/internal/test/get_logic_test.go
+++ service/user/rpc/internal/test/get_logic_test.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"log"
 	"net"
 	"testing"
 
-	"github.com/emicklei/go-restful/v3/log"
+	"github.com/google/uuid"
+	"github.com/linehk/go-microservices-blogger/errcode"
 	"github.com/linehk/go-microservices-blogger/service/user/rpc/internal/config"
+	"github.com/linehk/go-microservices-blogger/service/user/rpc/internal/logic"
 	"github.com/linehk/go-microservices-blogger/service/user/rpc/internal/server"
 	"github.com/linehk/go-microservices-blogger/service/user/rpc/internal/svc"
+	"github.com/linehk/go-microservices-blogger/service/user/rpc/model"
 	"github.com/linehk/go-microservices-blogger/service/user/rpc/user"
 	"github.com/zeromicro/go-zero/core/conf"
+	"go.uber.org/mock/gomock"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/test/bufconn"
@@ -20,7 +25,7 @@ import (
 
 var configFile = flag.String("f", "../../etc/user_test.yaml", "the config file")
 
-func serverStart(ctx context.Context) (user.UserServiceClient, func()) {
+func startServer(ctx context.Context) (user.UserServiceClient, func()) {
 	buffer := 101024 * 1024
 	lis := bufconn.Listen(buffer)
 
@@ -61,10 +66,10 @@ func serverStart(ctx context.Context) (user.UserServiceClient, func()) {
 	return client, closer
 }
 
-func TGet(t *testing.T) {
+func IntegrationTestGet(t *testing.T) {
 	ctx := context.Background()
 
-	client, closer := serverStart(ctx)
+	client, closer := startServer(ctx)
 	defer closer()
 
 	tests := []struct {
@@ -97,5 +102,30 @@ func TGet(t *testing.T) {
 		if got != tt.want {
 			t.Errorf("got %v, want %v", got, tt.want)
 		}
+	}
+}
+
+func newCtrl(t *testing.T) (*gomock.Controller, *model.MockAppUserModel, context.Context, *logic.GetLogic) {
+	ctrl := gomock.NewController(t)
+	repo := model.NewMockAppUserModel(ctrl)
+	ctx := context.Background()
+	logicService := logic.NewGetLogic(ctx, &svc.ServiceContext{
+		AppUserModel: repo,
+	})
+	return ctrl, repo, ctx, logicService
+}
+
+func TestGetUserNotExist(t *testing.T) {
+	ctrl, repo, ctx, logicService := newCtrl(t)
+	defer ctrl.Finish()
+
+	userId := uuid.New().String()
+
+	repo.EXPECT().FindOneByUuid(ctx, userId).Return(nil, model.ErrNotFound)
+
+	_, gotErr := logicService.Get(&user.GetReq{UserId: userId})
+	wantErr := errcode.Wrap(errcode.UserNotExist)
+	if !errors.Is(gotErr, wantErr) {
+		t.Errorf("got %v, want %v", gotErr, wantErr)
 	}
 }
