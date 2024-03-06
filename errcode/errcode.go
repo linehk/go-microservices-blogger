50f3f1d40f1a84ec75b2e9c8dbca0e3e6747dac0
sulinehk <sulinehk@gmail.com>
2024-03-06 13:24:26 +0800

    feat(post): add PostUserInfosNotExist

--- errcode/errcode.go
+++ errcode/errcode.go
@@ -16,12 +16,13 @@ const (
 	BlogUserInfoNotExist uint32 = 40001
 	PageViewNotExist     uint32 = 40002
 
-	PostNotExist        uint32 = 50000
-	PostNotBelongToBlog uint32 = 50001
-	ImageNotExist       uint32 = 50002
-	AuthorNotExist      uint32 = 50003
-	LabelNotExist       uint32 = 50004
-	LocationNotExist    uint32 = 50005
+	PostNotExist          uint32 = 50000
+	PostNotBelongToBlog   uint32 = 50001
+	ImageNotExist         uint32 = 50002
+	AuthorNotExist        uint32 = 50003
+	LabelNotExist         uint32 = 50004
+	LocationNotExist      uint32 = 50005
+	PostUserInfosNotExist uint32 = 50006
 )
 
 var msg = map[uint32]string{
@@ -35,12 +36,13 @@ var msg = map[uint32]string{
 	BlogUserInfoNotExist: "blog user info not exist",
 	PageViewNotExist:     "page view not exist",
 
-	PostNotExist:        "post not exist",
-	PostNotBelongToBlog: "post not belong to blog",
-	ImageNotExist:       "image not exist",
-	AuthorNotExist:      "author not exist",
-	LabelNotExist:       "label not exist",
-	LocationNotExist:    "location not exist",
+	PostNotExist:          "post not exist",
+	PostNotBelongToBlog:   "post not belong to blog",
+	ImageNotExist:         "image not exist",
+	AuthorNotExist:        "author not exist",
+	LabelNotExist:         "label not exist",
+	LocationNotExist:      "location not exist",
+	PostUserInfosNotExist: "post user info not exist",
 }
 
 func Wrap(e uint32) error {
