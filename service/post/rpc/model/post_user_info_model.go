938d676b8dfd197f07abbecc5937d41c280dbb42
sulinehk <sulinehk@gmail.com>
2024-03-06 13:24:01 +0800

    feat(post): add GetPostUserInfos

--- service/post/rpc/model/post_user_info_model.go
+++ service/post/rpc/model/post_user_info_model.go
@@ -1,7 +1,11 @@
 package model
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlc"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
@@ -13,6 +17,7 @@ type (
 	// and implement the added methods in customPostUserInfoModel.
 	PostUserInfoModel interface {
 		postUserInfoModel
+		GetPostUserInfos(ctx context.Context, userUuid, blogUuid, postUuid string) (*PostUserInfo, error)
 	}
 
 	customPostUserInfoModel struct {
@@ -24,5 +29,29 @@ type (
 func NewPostUserInfoModel(conn sqlx.SqlConn, c cache.CacheConf, opts ...cache.Option) PostUserInfoModel {
 	return &customPostUserInfoModel{
 		defaultPostUserInfoModel: newPostUserInfoModel(conn, c, opts...),
+	}
+}
+
+var (
+	cachePublicPostUserInfoUserUuidBlogUuidPostUuid = "cache:public:postUserInfo:userUuid:%s:blogUuid:%s:postUuid:%s"
+)
+
+func (c *customPostUserInfoModel) GetPostUserInfos(ctx context.Context, userUuid, blogUuid, postUuid string) (*PostUserInfo, error) {
+	publicPostUserInfoBlogUuidKey := fmt.Sprintf(cachePublicPostUserInfoUserUuidBlogUuidPostUuid, userUuid, blogUuid, postUuid)
+	var resp PostUserInfo
+	err := c.QueryRowIndexCtx(ctx, &resp, publicPostUserInfoBlogUuidKey, c.formatPrimary, func(ctx context.Context, conn sqlx.SqlConn, v any) (i any, e error) {
+		query := fmt.Sprintf("select %s from %s where user_uuid = $1 and blog_uuid = $2 and post_uuid = $3 limit 1", postUserInfoRows, c.table)
+		if err := conn.QueryRowCtx(ctx, &resp, query, userUuid, blogUuid, postUuid); err != nil {
+			return nil, err
+		}
+		return resp.Id, nil
+	}, c.queryPrimary)
+	switch err {
+	case nil:
+		return &resp, nil
+	case sqlc.ErrNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
 	}
 }
