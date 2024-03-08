3aecc5e67d5bbfc1616ae06e0e6d3e8cea9774d4
sulinehk <sulinehk@gmail.com>
2024-03-08 19:40:10 +0800

    feat(post): add Publish

--- service/post/rpc/internal/logic/publish_logic.go
+++ service/post/rpc/internal/logic/publish_logic.go
@@ -2,8 +2,11 @@ package logic
 
 import (
 	"context"
+	"errors"
 
+	"github.com/linehk/go-microservices-blogger/errcode"
 	"github.com/linehk/go-microservices-blogger/service/post/rpc/internal/svc"
+	"github.com/linehk/go-microservices-blogger/service/post/rpc/model"
 	"github.com/linehk/go-microservices-blogger/service/post/rpc/post"
 
 	"github.com/zeromicro/go-zero/core/logx"
@@ -24,7 +27,21 @@ func NewPublishLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PublishLo
 }
 
 func (l *PublishLogic) Publish(in *post.PublishReq) (*post.Post, error) {
-	// todo: add your logic here and delete this line
+	postModel, err := l.svcCtx.PostModel.FindOneByBlogUuidAndPostUuid(l.ctx, in.GetBlogId(), in.GetPostId())
+	if errors.Is(err, model.ErrNotFound) {
+		l.Error(errcode.Msg(errcode.PostNotExist))
+		return nil, errcode.Wrap(errcode.PostNotExist)
+	}
+	if err != nil {
+		l.Error(errcode.Msg(errcode.Database))
+		return nil, errcode.Wrap(errcode.Database)
+	}
+	postModel.Published.Valid = true
+	postModel.Published.Time = in.GetPublishDate().AsTime()
 
-	return &post.Post{}, nil
+	if err := l.svcCtx.PostModel.Update(l.ctx, postModel); err != nil {
+		return nil, err
+	}
+
+	return Get(l.ctx, l.svcCtx, l.Logger, postModel)
 }
