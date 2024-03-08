a2e894af02767cdf878c70620aba19d930997a23
sulinehk <sulinehk@gmail.com>
2024-03-08 19:25:06 +0800

    feat(post): add Patch

--- service/post/rpc/internal/logic/patch_logic.go
+++ service/post/rpc/internal/logic/patch_logic.go
@@ -2,8 +2,11 @@ package logic
 
 import (
 	"context"
+	"database/sql"
 
+	"github.com/linehk/go-microservices-blogger/errcode"
 	"github.com/linehk/go-microservices-blogger/service/post/rpc/internal/svc"
+	"github.com/linehk/go-microservices-blogger/service/post/rpc/model"
 	"github.com/linehk/go-microservices-blogger/service/post/rpc/post"
 
 	"github.com/zeromicro/go-zero/core/logx"
@@ -24,7 +27,24 @@ func NewPatchLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PatchLogic 
 }
 
 func (l *PatchLogic) Patch(in *post.PatchReq) (*post.Post, error) {
-	// todo: add your logic here and delete this line
-
-	return &post.Post{}, nil
+	postReq := in.GetPost()
+	postModel := &model.Post{
+		Uuid:           in.GetPostId(),
+		BlogUuid:       sql.NullString{String: in.GetBlogId(), Valid: true},
+		Published:      sql.NullTime{Time: postReq.GetPublished().AsTime(), Valid: true},
+		Updated:        sql.NullTime{Time: postReq.GetUpdated().AsTime(), Valid: true},
+		Url:            postReq.GetUrl(),
+		SelfLink:       sql.NullString{String: postReq.GetSelfLink(), Valid: true},
+		Title:          sql.NullString{String: postReq.GetTitle(), Valid: true},
+		TitleLink:      sql.NullString{String: postReq.GetTitleLink(), Valid: true},
+		Content:        sql.NullString{String: postReq.GetContent(), Valid: true},
+		CustomMetaData: sql.NullString{String: postReq.GetCustomMetaData(), Valid: true},
+		Status:         sql.NullString{String: postReq.GetStatus(), Valid: true},
+	}
+	if err := l.svcCtx.PostModel.Update(l.ctx, postModel); err != nil {
+		l.Error(errcode.Msg(errcode.Database))
+		return nil, errcode.Wrap(errcode.Database)
+	}
+	
+	return Get(l.ctx, l.svcCtx, l.Logger, postModel)
 }
